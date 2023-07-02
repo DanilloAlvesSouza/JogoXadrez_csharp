9fc3f294140756ab357b6f421eaa290c8764fc0d
DanilloAlvesSouza <danillo.alves.souza@outlook.com>
2023-07-02 12:53:22 -0300

    Update a implementacao do metodo imprimir tabuleiro

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -6,23 +6,18 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-
-        PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-        Console.WriteLine(pos);
-        Console.WriteLine(pos.toPosicao());
-        //try
-        //{
-        //    Tabuleiro tab = new Tabuleiro(8, 8);
-        //    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-        //    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-        //    tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(1, 3));
-        //    tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-        //    Tela.imprimirTabuleiro(tab);
-        //    Console.ReadLine();
-        //}
-        //catch (TabuleiroException e)
-        //{
-        //    Console.WriteLine(e.Message);
-        //}
+        try
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 4));
+            Tela.imprimirTabuleiro(tab);
+            Console.ReadLine();
+        }
+        catch (TabuleiroException e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
