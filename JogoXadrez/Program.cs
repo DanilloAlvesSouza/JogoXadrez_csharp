bf801e91532898ce98f6c1932e93a8c6ff8c2900
DanilloAlvesSouza <danillo.alves.souza@outlook.com>
2023-07-02 20:29:08 -0300

    Implementando as restrições de movimento

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -14,25 +14,39 @@ internal class Program
 
             while (!partida.Terminada)
             {
-                Console.Clear();
-                Tela.imprimirTabuleiro(partida.Tab);
+                try
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.Tab);
+                    Console.WriteLine();
+                    Console.WriteLine("Turno: " + partida.Turno);
+                    Console.WriteLine("Aguardando jogada: " + partida.JogadorAtual);
 
-                Console.WriteLine();
-                Console.Write("Origem: ");
-                Posicao origem = Tela.LerPosicaoXadrez().toPosicao();
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.LerPosicaoXadrez().toPosicao();
+                    partida.ValidarPosicaoOrigem(origem);
 
-                bool[,] posicoesPossiveis = partida.Tab.peca(origem).MovimentosPosiveis();
+                    bool[,] posicoesPossiveis = partida.Tab.peca(origem).MovimentosPosiveis();
 
-                Console.Clear();
-                Tela.imprimirTabuleiro(partida.Tab,posicoesPossiveis);
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.Tab, posicoesPossiveis);
 
-                Console.WriteLine();
-                Console.Write("Destino: ");
-                Posicao destino = Tela.LerPosicaoXadrez().toPosicao();
+                    Console.WriteLine();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.LerPosicaoXadrez().toPosicao();
+                    partida.ValidarPosicaoDestino(origem, destino);
 
-                partida.ExecutaMovimento(origem, destino);
-            }                     
-            
+                    partida.RealizaJogada(origem, destino);
+                }
+                catch (TabuleiroException e)
+                { 
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }
+                
+
+            }
         }
         catch (TabuleiroException e)
         {
--- JogoXadrez/Tabuleiro/Peca.cs
+++ JogoXadrez/Tabuleiro/Peca.cs
@@ -16,13 +16,36 @@
             Cor = cor;
             QteMovimento = 0;
         }
-        public abstract bool[,] MovimentosPosiveis();
-        
-
         public void IncrementaQteMovimentos()
         {
             QteMovimento++;
         }
+        public bool ExisteMovimentosPossiveis()
+        {
+            bool[,] mat = MovimentosPosiveis();
+            for(int i = 0;i<tab.linhas;i++)
+            {
+                for(int j = 0; j<tab.colunas;j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+
+        }
+
+        public bool PodeMoverPara(Posicao pos)
+        {
+            return MovimentosPosiveis()[pos.Linha, pos.Coluna];
+        }
+
+        public abstract bool[,] MovimentosPosiveis();
+        
+
+       
 
        
     }
